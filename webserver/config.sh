e3da1b393777c7943aeaca9c0855ba90e696e69e
Trevor256 <79394682+trevor256@users.noreply.github.com>
2022-06-05 14:01:25 -0500

    Update config.sh

--- webserver/config.sh
+++ webserver/config.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 sudo apt-get update -y && sudo apt-get upgrade -y
-sudo apt install ufw -y
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+sudo apt install ufw rustc cargo -y
+cargo install trunk
+rustup target add wasm32-unknown-unknown
 sudo ufw allow proto tcp to 0.0.0.0/0 port 22
 sudo ufw allow proto tcp to 0.0.0.0/0 port 443
 echo "y" | sudo ufw enable
